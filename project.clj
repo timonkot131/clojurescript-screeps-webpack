df855f8a19aa71b0b82e60fd8a06727d1e12cbe5
timonkot131 <timonoid01@gmail.com>
2022-08-26 19:07:07 +0400

    changed description and license info in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.timonkot13/clojurescript-screeps-webpack "0.1.0"
-  :description ""
+  :description "Clojurescript bundler template for screeps"
   :user {:signing {:gpg-key "F46ED3F0E185C8FC037927AB0774E9980F3B2D92"}}
   :url "https://github.com/timonkot131/clojurescript-screeps-webpack"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
