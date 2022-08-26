3d2b060112afdf76893afbf9e5ab8ae3bae89893
timonkot131 <timonoid01@gmail.com>
2022-08-26 17:58:41 +0400

    added to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject org.clojars.timonkot13/lein-template.clojurescript-screeps-webpack "0.1.0"
+(defproject org.clojars.timonkot13/clojurescript-screeps-webpack "0.1.0"
   :description ""
-  :url "http://example.com/FIXME"
+  :user {:signing {:gpg-key "F46ED3F0E185C8FC037927AB0774E9980F3B2D92"}}
+  :url "https://github.com/timonkot131/clojurescript-screeps-webpack"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
