a300177808522bb051b4dfde2869a64eb270c53f
timonkot131 <timonoid01@gmail.com>
2022-12-27 15:38:29 +0400

    changed project name

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject org.clojars.timonkot13/lein-template.clojurescript-screeps-webpack "0.1.1"
-  :description "Clojurescript bundler template for screeps"
-  :user {:signing {:gpg-key "F46ED3F0E185C8FC037927AB0774E9980F3B2D92"}}
+(defproject clojurescript-screeps-webpack/lein-template "0.1.1"
+  :descripztion "Clojurescript bundler template for screeps"
   :url "https://github.com/timonkot131/clojurescript-screeps-webpack"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
